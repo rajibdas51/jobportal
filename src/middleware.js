3acc5fd6a687af54b4258628ea88b6494660bf52
Rajib <prorajib6@gmail.com>
2025-02-03 23:56:49 +0600

    add middleware

---
+++ src/middleware.js
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(req) {
+  const token = req.cookies.get('token');
+  const { pathname } = req.nextUrl;
+  // if user is not logged in and tries to access protected routes,redirect to login page
+  if (!token && pathname !== '/login' && pathname !== '/register') {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  // If user is logged in redirect to home page
+  if (token && (pathname === '/login' || pathname === '/register')) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  return NextResponse.next(); // Continue to the next middleware
+}
+
+export const config = {
+  matcher: [
+    '/dashboard/:path*',
+    '/jobs/:path*',
+    '/profile/:path*',
+    '/login',
+    '/register',
+  ],
+};
