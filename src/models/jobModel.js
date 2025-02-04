d63125480528e949115fde956ef3108a3e7d5506
Rajib <prorajib6@gmail.com>
2025-02-04 19:30:50 +0600

    create userModel

--- src/models/jobModel.js
+++
@@ -1,9 +0,0 @@
-import { Phone } from '@mui/icons-material';
-import mongoose from 'mongoose';
-
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  Phone: { type: String },
-});
